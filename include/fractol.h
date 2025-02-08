534b82010903d2e72bd21e1d91bd986ae97ab246
ynprgz <yanelperaltarodriguez77@gmail.com>
2025-02-08 16:36:12 +0100

    estructura

---
+++ include/fractol.h
@@ -0,0 +1,13 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+#include "../MLX42/include/MLX42/MLX42.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define HEIGHT 800
+#define WIDTH 800
+
+
+#endif
