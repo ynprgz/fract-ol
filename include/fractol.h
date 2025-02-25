e632b1551cd064e19486821813199292b3ccad93
ynprgz <yanelperaltarodriguez77@gmail.com>
2025-02-25 14:28:43 +0100

    updated: fractol.h

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 22:09:51 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/02/19 09:18:10 by yaperalt         ###   ########.fr       */
+/*   Updated: 2025/02/25 13:26:01 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 # include <unistd.h>
 # include <math.h>
 
-# define HEIGHT 800
-# define WIDTH 800
+# define HEIGHT 700
+# define WIDTH 700
 
 typedef struct s_data
 {
@@ -46,6 +46,7 @@ typedef struct s_data
 	double	z_im;
 	double	c_re;
 	double	c_im;
+	double	move_factor;
 }	t_data;
 
 // utils.c
@@ -53,6 +54,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	set(t_data *f, char *one, char *two);
 void	put_message(void);
+int		exit_program(t_data *f);
 
 // print_pixels.c
 void	put_pixel(t_data *f, int x, int y, int color);
@@ -71,5 +73,16 @@ void	burningship(t_data *f);
 
 // pixel_to_complexplane.c
 void	pixel_to_complexplane(t_data *f);
+void	set_coord(t_data *f);
+
+// events.c
+int		handle_keys(int keycode, t_data *f);
+int		handle_close(t_data *f);
+int		handle_mouse(int keycode, int x, int y, t_data *f);
+void	zoom(t_data *f, int mouse_x, int mouse_y, double zoom_factor);
+int		handle_arrows(int keycode, t_data *f);
+
+// check_args.c
+int		check_args(int argc, char **argv);
 
 #endif
